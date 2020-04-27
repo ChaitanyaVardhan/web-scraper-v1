79f2039534568a01dae5060f61687f9402f56463
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-04-27 13:14:50 +0000

    web-scraper:wip

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template, jsonify, session, redirect, url_for
+import boto3
 import requests
+import os
+import json
 import re, uuid
 
 app = Flask(
@@ -8,6 +11,8 @@ app = Flask(
     template_folder='templates'
 )
 app.config['SECRET_KEY'] = "FOO"
+
+Cache = {}
 
 @app.before_request
 def verify_session_id():
@@ -33,6 +38,11 @@ def scrape():
         for anchor in anchors:
             match_obj = re.search(href_pattern, anchor)
             hrefs.append(match_obj.group(1))
+        file_name = '_'.join(url.split("//")[1].split('/'))
+        Cache[session['session_id']] = dict(
+            file_name = file_name,
+            data = hrefs
+        )
         return jsonify(hrefs)
     else:
         return render_template('index.html')
@@ -40,6 +50,13 @@ def scrape():
 @app.route('/save')
 def save_scrape():
     if 'session_id' in session:
+        s3 = boto3.resource('s3')
+        s3.Bucket(
+            os.environ.get("BUCKET_NAME")
+        ).put_object(
+            Key=f"{session['session_id']}/{Cache[session['session_id']]['file_name']}.json",
+            Body=json.dumps(Cache[session['session_id']]['data'])
+        )
         return jsonify({'status': 'saved'})
     else:
         return redirect('/')
